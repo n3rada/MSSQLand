27a4a49e0723bb0afabfb36637fb2c62855617aa
n3rada <72791564+n3rada@users.noreply.github.com>
2025-02-25 19:36:16 +0100

    Modify `Impersonation.cs` to only use `DataTable`

--- MSSQLand/Actions/Database/Impersonation.cs
+++ MSSQLand/Actions/Database/Impersonation.cs
@@ -1,9 +1,7 @@
 ﻿using MSSQLand.Services;
 using MSSQLand.Utilities;
 using System;
-using System.Collections.Generic;
 using System.Data;
-
 
 namespace MSSQLand.Actions.Database
 {
@@ -18,8 +16,6 @@ namespace MSSQLand.Actions.Database
         {
             Logger.TaskNested("Starting impersonation check");
 
-            Dictionary<string, string> allLogins = new();
-
             // Query to obtain all SQL logins and Windows principals
             string query = "SELECT name FROM sys.server_principals WHERE type_desc IN ('SQL_LOGIN', 'WINDOWS_LOGIN') AND name NOT LIKE '##%';";
             DataTable queryResult = databaseContext.QueryService.ExecuteTable(query);
@@ -27,8 +23,16 @@ namespace MSSQLand.Actions.Database
             if (queryResult.Rows.Count == 0)
             {
                 Logger.Warning("No SQL logins or Windows principals found.");
-                return allLogins;
+                return queryResult;
             }
+
+            // Add a writable column for impersonation status
+            DataColumn impersonationColumn = new("Impersonation", typeof(string))
+            {
+                DefaultValue = "No"
+            };
+
+            queryResult.Columns.Add(impersonationColumn);
 
             // Check if the current user is a sysadmin
             if (databaseContext.UserService.IsAdmin())
@@ -36,8 +40,7 @@ namespace MSSQLand.Actions.Database
                 Logger.Success("Current user is 'sysadmin'; it can impersonate any login.");
                 foreach (DataRow row in queryResult.Rows)
                 {
-                    string user = row["name"].ToString();
-                    allLogins.Add(user, "Yes");
+                    row["Impersonation"] = "Yes";
                 }
             }
             else
@@ -47,14 +50,13 @@ namespace MSSQLand.Actions.Database
                 {
                     string user = row["name"].ToString();
                     bool canImpersonate = databaseContext.UserService.CanImpersonate(user);
-                    allLogins.Add(user, canImpersonate ? "Yes" : "No");
+                    row["Impersonation"] = canImpersonate ? "Yes" : "No";
                 }
             }
 
+            Console.WriteLine(MarkdownFormatter.ConvertDataTableToMarkdownTable(queryResult));
 
-            Console.WriteLine(MarkdownFormatter.ConvertDictionaryToMarkdownTable(allLogins, "Logins", "Impersonation"));
-
-            return allLogins;
+            return queryResult;
         }
     }
 }
