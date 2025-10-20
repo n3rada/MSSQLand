cab4afcabfbd193c888b07cab025572add4925e7
n3rada <72791564+n3rada@users.noreply.github.com>
2025-10-20 21:39:22 +0200

    Refactor user queries in Users class: separate server principals and database users retrieval with improved logging

--- MSSQLand/Actions/Database/Users.cs
+++ MSSQLand/Actions/Database/Users.cs
@@ -14,17 +14,33 @@ namespace MSSQLand.Actions.Database
 
         public override object? Execute(DatabaseContext databaseContext)
         {
+            Logger.Info("Server principals with role memberships");
+
+            string serverPrincipalsQuery = @"
+                SELECT r.name AS Name, r.type_desc AS Type, r.is_disabled, r.create_date, r.modify_date,
+                       sl.sysadmin, sl.securityadmin, sl.serveradmin, sl.setupadmin, 
+                       sl.processadmin, sl.diskadmin, sl.dbcreator, sl.bulkadmin 
+                FROM master.sys.server_principals r 
+                LEFT JOIN master.sys.syslogins sl ON sl.sid = r.sid 
+                WHERE r.type IN ('G','U','E','S','X') AND r.name NOT LIKE '##%'
+                ORDER BY r.modify_date DESC;";
+
+            Console.WriteLine(MarkdownFormatter.ConvertDataTableToMarkdownTable(
+                databaseContext.QueryService.ExecuteTable(serverPrincipalsQuery)));
+
             Logger.Info("Database users");
 
+            string databaseUsersQuery = @"
+                SELECT name AS username, create_date, modify_date, type_desc AS type, 
+                       authentication_type_desc AS authentication_type 
+                FROM sys.database_principals 
+                WHERE type NOT IN ('R', 'A', 'X') AND sid IS NOT null AND name NOT LIKE '##%' 
+                ORDER BY modify_date DESC;";
 
-            Console.WriteLine(MarkdownFormatter.ConvertDataTableToMarkdownTable(databaseContext.QueryService.ExecuteTable("SELECT name AS username, create_date, modify_date, type_desc AS type, authentication_type_desc AS authentication_type FROM sys.database_principals WHERE type NOT IN ('A', 'R', 'X') AND sid IS NOT null AND name NOT LIKE '##%' ORDER BY modify_date DESC;")));
-
-            Logger.Info("Server principals");
-
-            Console.WriteLine(MarkdownFormatter.ConvertDataTableToMarkdownTable(databaseContext.QueryService.ExecuteTable("SELECT name, type_desc, is_disabled, create_date, modify_date FROM sys.server_principals WHERE name NOT LIKE '##%' ORDER BY modify_date DESC;")));
+            Console.WriteLine(MarkdownFormatter.ConvertDataTableToMarkdownTable(
+                databaseContext.QueryService.ExecuteTable(databaseUsersQuery)));
 
             return null;
-
         }
     }
 }
