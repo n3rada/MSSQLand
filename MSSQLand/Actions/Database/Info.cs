c301b9b3b2b741d140d4b8b94f5cc9db88e5e63e
n3rada <72791564+n3rada@users.noreply.github.com>
2025-11-02 16:59:34 +0100

    Refactor query dictionary syntax for consistency and clarity

--- MSSQLand/Actions/Database/Info.cs
+++ MSSQLand/Actions/Database/Info.cs
@@ -16,21 +16,21 @@ namespace MSSQLand.Actions.Database
         [ExcludeFromArguments]
         private readonly Dictionary<string, string> _queries = new()
         {
-            "Server Name": "SELECT @@SERVERNAME;",
-            "Default Domain": "SELECT DEFAULT_DOMAIN();",
-            "Host Name": "SELECT CAST(SERVERPROPERTY('MachineName') AS NVARCHAR(256));",
-            "Operating System Version": "SELECT TOP(1) windows_release + ISNULL(' ' + windows_service_pack_level, '') FROM sys.dm_os_windows_info;",
-            "SQL Service Process ID": "SELECT CAST(SERVERPROPERTY('ProcessId') AS INT);",
-            "SQL Service Account": "SELECT CAST(SERVERPROPERTY('ServiceAccountName') AS NVARCHAR(256));",
-            "Instance Name": "SELECT ISNULL(CAST(SERVERPROPERTY('InstanceName') AS NVARCHAR(256)), 'DEFAULT');",
-            "Authentication Mode": "SELECT CASE CAST(SERVERPROPERTY('IsIntegratedSecurityOnly') AS INT) WHEN 1 THEN 'Windows Authentication only' ELSE 'Mixed mode (Windows + SQL)' END;",
-            "Clustered Server": "SELECT CASE CAST(SERVERPROPERTY('IsClustered') AS INT) WHEN 0 THEN 'No' ELSE 'Yes' END;",
-            "SQL Version": "SELECT CAST(SERVERPROPERTY('ProductVersion') AS NVARCHAR(256));",
-            "SQL Major Version": "SELECT SUBSTRING(@@VERSION, CHARINDEX('2', @@VERSION), 4);",
-            "SQL Edition": "SELECT CAST(SERVERPROPERTY('Edition') AS NVARCHAR(256));",
-            "SQL Service Pack": "SELECT CAST(SERVERPROPERTY('ProductLevel') AS NVARCHAR(256));",
-            "OS Architecture": "SELECT SUBSTRING(@@VERSION, CHARINDEX('x', @@VERSION), 3);",
-            "OS Version Number": "SELECT RIGHT(SUBSTRING(@@VERSION, CHARINDEX('Windows Server', @@VERSION), 19), 4);",
+            { "Server Name", "SELECT @@SERVERNAME;" },
+            { "Default Domain", "SELECT DEFAULT_DOMAIN();" },
+            { "Host Name", "SELECT CAST(SERVERPROPERTY('MachineName') AS NVARCHAR(256));" },
+            { "Operating System Version", "SELECT TOP(1) windows_release + ISNULL(' ' + windows_service_pack_level, '') FROM sys.dm_os_windows_info;" },
+            { "SQL Service Process ID", "SELECT CAST(SERVERPROPERTY('ProcessId') AS INT);" },
+            { "SQL Service Account", "SELECT CAST(SERVERPROPERTY('ServiceAccountName') AS NVARCHAR(256));" },
+            { "Instance Name", "SELECT ISNULL(CAST(SERVERPROPERTY('InstanceName') AS NVARCHAR(256)), 'DEFAULT');" },
+            { "Authentication Mode", "SELECT CASE CAST(SERVERPROPERTY('IsIntegratedSecurityOnly') AS INT) WHEN 1 THEN 'Windows Authentication only' ELSE 'Mixed mode (Windows + SQL)' END;" },
+            { "Clustered Server", "SELECT CASE CAST(SERVERPROPERTY('IsClustered') AS INT) WHEN 0 THEN 'No' ELSE 'Yes' END;" },
+            { "SQL Version", "SELECT CAST(SERVERPROPERTY('ProductVersion') AS NVARCHAR(256));" },
+            { "SQL Major Version", "SELECT SUBSTRING(@@VERSION, CHARINDEX('2', @@VERSION), 4);" },
+            { "SQL Edition", "SELECT CAST(SERVERPROPERTY('Edition') AS NVARCHAR(256));" },
+            { "SQL Service Pack", "SELECT CAST(SERVERPROPERTY('ProductLevel') AS NVARCHAR(256));" },
+            { "OS Architecture", "SELECT SUBSTRING(@@VERSION, CHARINDEX('x', @@VERSION), 3);" },
+            { "OS Version Number", "SELECT RIGHT(SUBSTRING(@@VERSION, CHARINDEX('Windows Server', @@VERSION), 19), 4);" },
         };
 
         public override void ValidateArguments(string additionalArguments)
