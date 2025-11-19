949533cfc2254a0c817faa3c2193089b06938172
n3rada <72791564+n3rada@users.noreply.github.com>
2025-11-19 15:53:10 +0000

    fix: Enhance user enumeration logic to differentiate between Azure SQL and on-premises SQL Server

--- MSSQLand/Actions/Database/Users.cs
+++ MSSQLand/Actions/Database/Users.cs
@@ -27,18 +27,54 @@ namespace MSSQLand.Actions.Database
 
         public override object? Execute(DatabaseContext databaseContext)
         {
+            bool isAzureSQL = databaseContext.QueryService.IsAzureSQL();
+
+            if (isAzureSQL)
+            {
+                // Azure SQL Database: Only show database users (no server-level principals access)
+                Logger.Info("Enumerating database users in current database context");
+                Logger.InfoNested("Note: Server-level principals not accessible on Azure SQL Database");
+                Logger.NewLine();
+
+                string databaseUsersQuery = @"
+                    SELECT name AS username, create_date, modify_date, type_desc AS type, 
+                           authentication_type_desc AS authentication_type 
+                    FROM sys.database_principals 
+                    WHERE type NOT IN ('R', 'A', 'X') AND sid IS NOT null AND name NOT LIKE '##%' 
+                    ORDER BY modify_date DESC;";
+
+                Console.WriteLine(OutputFormatter.ConvertDataTable(
+                    databaseContext.QueryService.ExecuteTable(databaseUsersQuery)));
+
+                return null;
+            }
+
+            // On-premises SQL Server: Show server logins and database users
             Logger.Info("Enumerating server-level principals (logins) and their instance-wide server roles");
             Logger.InfoNested("Note: Use 'roles' action to see database-level role memberships");
             Logger.NewLine();
 
             string query = @"
-                SELECT r.name AS Name, r.type_desc AS Type, r.is_disabled, r.create_date, r.modify_date,
-                       sl.sysadmin, sl.securityadmin, sl.serveradmin, sl.setupadmin, 
-                       sl.processadmin, sl.diskadmin, sl.dbcreator, sl.bulkadmin 
-                FROM master.sys.server_principals r 
-                LEFT JOIN master.sys.syslogins sl ON sl.sid = r.sid 
-                WHERE r.type IN ('G','U','E','S','X') AND r.name NOT LIKE '##%'
-                ORDER BY r.modify_date DESC;";
+                SELECT 
+                    sp.name AS Name, 
+                    sp.type_desc AS Type, 
+                    sp.is_disabled, 
+                    sp.create_date, 
+                    sp.modify_date,
+                    MAX(CASE WHEN sr.name = 'sysadmin' THEN 1 ELSE 0 END) AS sysadmin,
+                    MAX(CASE WHEN sr.name = 'securityadmin' THEN 1 ELSE 0 END) AS securityadmin,
+                    MAX(CASE WHEN sr.name = 'serveradmin' THEN 1 ELSE 0 END) AS serveradmin,
+                    MAX(CASE WHEN sr.name = 'setupadmin' THEN 1 ELSE 0 END) AS setupadmin,
+                    MAX(CASE WHEN sr.name = 'processadmin' THEN 1 ELSE 0 END) AS processadmin,
+                    MAX(CASE WHEN sr.name = 'diskadmin' THEN 1 ELSE 0 END) AS diskadmin,
+                    MAX(CASE WHEN sr.name = 'dbcreator' THEN 1 ELSE 0 END) AS dbcreator,
+                    MAX(CASE WHEN sr.name = 'bulkadmin' THEN 1 ELSE 0 END) AS bulkadmin
+                FROM master.sys.server_principals sp
+                LEFT JOIN master.sys.server_role_members srm ON sp.principal_id = srm.member_principal_id
+                LEFT JOIN master.sys.server_principals sr ON srm.role_principal_id = sr.principal_id AND sr.type = 'R'
+                WHERE sp.type IN ('G','U','E','S','X') AND sp.name NOT LIKE '##%'
+                GROUP BY sp.name, sp.type_desc, sp.is_disabled, sp.create_date, sp.modify_date
+                ORDER BY sp.modify_date DESC;";
 
             DataTable rawTable = databaseContext.QueryService.ExecuteTable(query);
 
