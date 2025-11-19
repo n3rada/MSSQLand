62cf4499b22849b33cd8ff494ca71c04ada5f317
n3rada <72791564+n3rada@users.noreply.github.com>
2025-11-19 12:30:23 +0000

    fix: Update SQL queries for major version and OS details to use SERVERPROPERTY for accuracy

--- MSSQLand/Actions/Database/Info.cs
+++ MSSQLand/Actions/Database/Info.cs
@@ -26,11 +26,11 @@ namespace MSSQLand.Actions.Database
             { "Authentication Mode", "SELECT CASE SERVERPROPERTY('IsIntegratedSecurityOnly') WHEN 1 THEN 'Windows Authentication only' ELSE 'Mixed mode (Windows + SQL)' END;" },
             { "Clustered Server", "SELECT CASE SERVERPROPERTY('IsClustered') WHEN 0 THEN 'No' ELSE 'Yes' END;" },
             { "SQL Version", "SELECT SERVERPROPERTY('ProductVersion');" },
-            { "SQL Major Version", "SELECT SUBSTRING(@@VERSION, CHARINDEX('2', @@VERSION), 4);" },
+            { "SQL Major Version", "SELECT SERVERPROPERTY('ProductMajorVersion');" },
             { "SQL Edition", "SELECT SERVERPROPERTY('Edition');" },
             { "SQL Service Pack", "SELECT SERVERPROPERTY('ProductLevel');" },
-            { "OS Architecture", "SELECT SUBSTRING(@@VERSION, CHARINDEX('x', @@VERSION), 3);" },
-            { "OS Version Number", "SELECT RIGHT(SUBSTRING(@@VERSION, CHARINDEX('Windows Server', @@VERSION), 19), 4);" },
+            { "OS Architecture", "SELECT CASE WHEN CAST(SERVERPROPERTY('Edition') AS NVARCHAR(128)) LIKE '%64%' THEN '64-bit' ELSE '32-bit' END;" },
+            { "OS Version Number", "SELECT @@VERSION;" },
         };
 
         public override void ValidateArguments(string additionalArguments)
