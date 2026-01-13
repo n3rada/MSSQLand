69dcac26b1d8b8c4dad2cc97d3933d90b3f00b85
n3rada <72791564+n3rada@users.noreply.github.com>
2026-01-13 23:51:49 +0000

    refactor(AssemblyInfo): clean up comments and restore versioning to 2.0.0

--- MSSQLand/Properties/AssemblyInfo.cs
+++ MSSQLand/Properties/AssemblyInfo.cs
@@ -1,8 +1,5 @@
 ﻿using System.Reflection;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("MSSQLand")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -12,25 +9,11 @@
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Assembly versioning follows the format:
-// 
-// Major
-// Breaking changes or major capability shift.
-// Changing this implies incompatibility with older versions.
+// Semantic Versioning 2.0.0 (https://semver.org/)
+// Format: MAJOR.MINOR.PATCH
+//   MAJOR - Breaking changes, incompatible API
+//   MINOR - New features, backward compatible
+//   PATCH - Bug fixes, small corrections
 //
-// Minor
-// New features, but backward compatible behavior.
-//
-// Build
-// Internal release iteration or milestone. Often incremented per stable release.
-//
-// Revision
-// Patch-level change: hotfixes, small internal corrections, minor tweaks.
-//
-// Example versions:
-// 2.0.0 - Initial public release
-// 2.1.0 - Added data access control and EAFP pattern
-// 2.1.1 - Bug fixes and performance improvements
-//
-[assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyFileVersion("2.0.0")]
