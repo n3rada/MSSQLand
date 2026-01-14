b1a07d5f8b59b3839b8b46b46916fb8e294b7554
n3rada <72791564+n3rada@users.noreply.github.com>
2026-01-14 13:24:34 +0000

    refactor(FindSqlServers): remove unused System.Net namespace import

--- MSSQLand/Utilities/Discovery/FindSqlServers.cs
+++ MSSQLand/Utilities/Discovery/FindSqlServers.cs
@@ -4,7 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
-using System.Net;
 using System.Security.Principal;
 
 namespace MSSQLand.Utilities.Discovery
