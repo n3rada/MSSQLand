bce9068b6e2e9f0b17f7bbded83747e7e089eff4
n3rada <72791564+n3rada@users.noreply.github.com>
2026-01-29 17:33:36 +0000

    refactor: add missing using directive for MSSQLand.Actions in Helper

--- MSSQLand/Utilities/Helper.cs
+++ MSSQLand/Utilities/Helper.cs
@@ -4,6 +4,7 @@ using System;
 using System.Data;
 using System.Linq;
 using System.Collections.Generic;
+using MSSQLand.Actions;
 using MSSQLand.Utilities.Formatters;
 
 namespace MSSQLand.Utilities
