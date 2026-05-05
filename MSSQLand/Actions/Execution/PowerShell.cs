cc760665c6258b6ee559e3b73e0a7b222467d702
n3rada <72791564+n3rada@users.noreply.github.com>
2026-05-05 09:24:27 +0000

    refactor(powershell): simplify logic

--- MSSQLand/Actions/Execution/PowerShell.cs
+++ MSSQLand/Actions/Execution/PowerShell.cs
@@ -2,13 +2,12 @@
 
 using MSSQLand.Services;
 using MSSQLand.Utilities;
-using System;
-using System.Text;
 
 namespace MSSQLand.Actions.Execution
 {
     internal class PowerShell : XpCmd
     {
+
         [ArgumentMetadata(Position = 0, Required = true, IsRemainder = true, Description = "PowerShell script or command to execute")]
         private string _script = "";
 
@@ -20,28 +19,9 @@ namespace MSSQLand.Actions.Execution
         {
             Logger.TaskNested($"Executing PowerShell script: {_script}");
 
-            // Convert the PowerShell script to Base64 encoding
-            string base64EncodedScript = ConvertToBase64(_script);
+            _command = $"powershell.exe -NonI -NoPro -encodedComm {Misc.ConvertToBase64(_script)}";
 
-            // Craft the PowerShell command to execute the Base64-encoded script
-            string powerShellCommand = $"powershell.exe -noni -NoLogo -e {base64EncodedScript}";
-
-            // Set the crafted PowerShell command as the _command in the parent class
-            _command = powerShellCommand;
-
-            // Call the parent's Execute method to execute the command
             return base.Execute(databaseContext);
-        }
-
-        /// <summary>
-        /// Converts a string to Base64 encoding.
-        /// </summary>
-        /// <param name="input">The input string to encode.</param>
-        /// <returns>The Base64-encoded string.</returns>
-        private string ConvertToBase64(string input)
-        {
-            byte[] inputBytes = Encoding.Unicode.GetBytes(input); // PowerShell expects UTF-16 (Unicode) encoding
-            return Convert.ToBase64String(inputBytes);
         }
     }
 }
